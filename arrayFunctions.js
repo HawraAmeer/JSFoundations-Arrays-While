fabe5e0ab562635c59adc22d717e4c80bf58898d
HawraAmeer <hwra.ameer97@gmail.com>
2021-01-11 13:28:35 +0300

    done yay '

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -10,6 +10,7 @@
  */
 function isArrayLengthOdd(numbers) {
   // Your code here
+  return numbers.length % 2 !== 0;
 }
 
 /**
@@ -24,6 +25,7 @@ function isArrayLengthOdd(numbers) {
  */
 function isArrayLengthEven(numbers) {
   // Your code here
+  return numbers.length % 2 === 0;
 }
 
 /**
@@ -36,6 +38,8 @@ function isArrayLengthEven(numbers) {
  */
 function addLailaToArray(instructors) {
   // Your code here
+  instructors.push("Laila");
+  return instructors;
 }
 
 /**
@@ -48,6 +52,7 @@ function addLailaToArray(instructors) {
  */
 function eliminateTeam(teams) {
   // Your code here
+  return teams.pop();
 }
 
 /**
@@ -62,8 +67,12 @@ function eliminateTeam(teams) {
  */
 function secondHalfOfArrayIfItIsEven(fruits) {
   // Your code here
+  if (fruits.length % 2 === 0) {
+    return fruits.slice(fruits.length / 2);
+  } else {
+    return [];
+  }
 }
-
 /**
  * youGottaCalmDown(shout):
  * - receives a string `shout`
@@ -80,6 +89,12 @@ function secondHalfOfArrayIfItIsEven(fruits) {
  */
 function youGottaCalmDown(shout) {
   // Your code here
+  let count = 0;
+  while (shout.endsWith("!")) {
+    count++;
+    shout = shout.slice(0, shout.length - 1);
+  }
+  return shout + "!";
 }
 
 module.exports = {
